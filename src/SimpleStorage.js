14875deae50349c36f45a5f98f4e8535327b2fa0
Monmon <syu24059@gmail.com>
2024-10-01 10:11:43 +0900

    Added description of this project

--- src/SimpleStorage.js
+++ src/SimpleStorage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import abi from "./SimpleStorage.abi.json";
 
-const contractAddress = "0x5327d271a06625FA7f966Def9A39fd10723855F6"; // Change to your contract address
+const contractAddress = "0x5327d271a06625FA7f966Def9A39fd10723855F6"; // The deployed contract address
 
 const SimpleStorage = () => {
   const [storageValue, setStorageValue] = useState("");
@@ -63,6 +63,27 @@ const SimpleStorage = () => {
 
   return (
     <div>
+      <p
+        style={{
+          maxWidth: "950px",
+          marginBottom: "20px",
+          fontSize: "18px",
+          color: "#adacac",
+          textAlign: "justify",
+        }}
+      >
+        This is a decentralized application (DApp) that allows users to store
+        and retrieve a value on the Ethereum blockchain. This smart contract has
+        been deployed on the Sepolia test network. The 《Current Value》
+        displayed shows the number currently stored in the contract. When the
+        user wishes to modify the number, they can enter the desired number in
+        the text box and click 《Set Value》. The webpage will then attempt to
+        connect to the user's Web3 wallet (such as MetaMask) on the ETH Sepolia
+        network. After a successful connection, the user can pay the gas fee and
+        store the new number. After a moment, the new number entered by the user
+        will be displayed in the 《Current Value》 section.
+      </p>
+
       <p>Current Value: {storageValue}</p>
       <input
         type="text"
